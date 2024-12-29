7dc2b856a00993976a9a024124e5153363c390de
stevenzheng2002 <stevenzheng2002@163.com>
2024-12-30 01:20:05 +0800

    fix bugs

--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include "../exercise.h"
+#include <cstring>
 
 // READ: 类模板 <https://zh.cppreference.com/w/cpp/language/class_template>
 
@@ -10,10 +11,10 @@ struct Tensor4D {
     Tensor4D(unsigned int const *shape_, T const *data_) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
-        std::memcpy(shape, shape_, 4 * sizeof(unsigned int));
+        memcpy(shape, shape_, 4 * sizeof(unsigned int));
         size = shape[0] * shape[1] * shape[2] * shape[3];
         data = new T[size];
-        std::memcpy(data, data_, size * sizeof(T));
+        memcpy(data, data_, size * sizeof(T));
     }
     ~Tensor4D() {
         delete[] data;
